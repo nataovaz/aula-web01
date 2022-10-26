2e6eb7951d3ce225c275e36159a2ff6ea1f05510
nataovaz <natanvaz27@gmail.com>
2022-10-26 19:15:02 -0300

    projeto api

---
+++ aulaBackEnd/index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const { Client } = require("pg");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const config = require("./config");
+
+// Middleware
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyparser.json());
+
+var conString = config.urlConnection;
+var client = new Client(conString);
+client.connect(function (err) {
+  if (err) {
+    return console.error("Não foi possível conectar ao banco.", err);
+  }
+  client.query("SELECT NOW()", function (err, result) {
+    if (err) {
+      return console.error("Erro ao executar a query.", err);
+    }
+    console.log(result.rows[0]);
+  });
+});
+
+// ROTAS
+app.get("/", (req, res) => {
+  console.log("Response ok.");
+  res.send("Ok – Servidor disponível.");
+});
+
+app.get("/usuarios", (req, res) => {
+  try {
+    client.query("SELECT * FROM Usuarios", function (err, result) {
+      if (err) {
+        return console.error("Erro ao executar a query do SELECT", err);
+      }
+      res.send(result.rows);
+      console.log("Chamou get usuarios");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/usuarios/:id/", (req, res) => {
+  try {
+    console.log("Chamou /:id " + req.params.id);
+    client.query(
+      "SELECT * FROM Usuarios WHERE id = $1",
+      [req.params.id],
+      function (err, result) {
+        if (err) {
+          return console.error("Erro ao executar a qry de SELECT id", err);
+        }
+        res.send(result.rows);
+        // console.log(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/usuarios/:id", (req, res) => {
+  try {
+    console.log("Chamou delete /:id " + req.params.id);
+    const id = req.params.id;
+    client.query(
+      "DELETE FROM Usuarios WHERE id = $1",
+      [id],
+      function (err, result) {
+        if (err) {
+          return console.error("Erro ao executar a qry de DELETE", err);
+        } else {
+          if (result.rowCount == 0) {
+            res.status(400).json({ info: "Registro não encontrado." });
+          } else {
+            res.status(200).json({ info: `Registro excluído. Código: ${id}` });
+          }
+        }
+        console.log(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(config.port, () =>
+  console.log("Servidor funcionando na porta " + config.port)
+);
